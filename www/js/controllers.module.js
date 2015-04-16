4ab489cf737ed18af45e63d0b9e6d252beab497a
Tonny Yan <yhppenny@gmail.com>
2015-04-16 12:51:51 +1000

    inital commint

---
+++ www/js/controllers.module.js
@@ -0,0 +1,27 @@
+(function(){
+  'use strict';
+
+  angular.module('controllerModule', []);
+
+  angular.module('controllerModule')
+      .controller('DashCtrl', function($scope) {})
+      .controller('ChatsCtrl', function($scope, Chats) {
+          $scope.chats = Chats.all();
+          $scope.remove = function(chat) {
+            Chats.remove(chat);
+          }
+      })
+      .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+          $scope.chat = Chats.get($stateParams.chatId);
+      })
+      .controller('AccountCtrl', function($scope) {
+        $scope.settings = {
+           enableFriends: true
+        };
+      });
+})();
+
+
+
+
+
